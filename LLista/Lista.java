778e1a212fb7630161d565679f947bea368cc78c
Crisctiann <150423871+CristianSco@users.noreply.github.com>
2024-03-15 09:48:15 +0000

    addAfter, cercato di fare remove pos e removevalue

--- LLista/Lista.java
+++ LLista/Lista.java
@@ -2,11 +2,44 @@ package LLista;
 
 public class Lista {
 
-    Nodo radice;
-    
-    public Lista() {
-        radice = null;
+
+    class Iteratore{
+
+        private Nodo nodo;
+
+        private Iteratore(Nodo nodo) {
+            this.nodo = nodo;
+        }
+        public boolean hasNext() {
+            return nodo!=null;
+        }
+        public Nodo next() {
+            if(nodo==null) return null;
+            //Nodo result = new Nodo(nodo.getValore(), nodo.getSuccessivo());
+            Nodo result = nodo;
+            nodo = nodo.getSuccessivo();
+            return result;   //nodo che restituiamo una copia
+        }
     }
+
+    Nodo radice;
+
+    public Lista() {
+    
+        radice = null;     
+           
+    }
+        public Iteratore getIterator(){
+
+            Iteratore i = new Iteratore(radice);
+            return i;
+
+        }
+
+        
+
+        
+    
 
     public boolean isEmpty() {
         return radice==null;
@@ -55,6 +88,76 @@ public class Lista {
         n.setSuccessivo(p2);
         p1.setSuccessivo(n);
     }
+
+   /* public boolean addAfter(Nodo n, int pos) {   senza iteratore
+        if (isEmpty()) {
+            return false;
+        } 
+        Nodo p1 = radice;
+        Nodo p2 = radice.getSuccessivo();
+        int i=0;
+        while ( p2!=null && i<pos-1) {
+            p1 = p2;
+            p2 = p1.getSuccessivo();
+            i++;
+        }
+        if( i!=pos-1 ) { // se supera la lunchezza della lista
+            return false;
+        }
+        n.setSuccessivo(p2);
+        p1.setSuccessivo(n);
+        return true;
+
+
+    }
+     */
+    public boolean addAfter(Nodo n, int pos){  //con iteratore
+        //aggiunge il nodo n solo dopo aver oltrepassato il nodo di indice
+        Iteratore iter= getIterator();
+        int i;
+        Nodo npos=null; // nodo posizione
+        for(i=0; i<pos; i++){
+            if(iter.hasNext())npos=iter.next();
+            else return false;
+        }
+        n.setSuccessivo(npos.getSuccessivo());
+        npos.setSuccessivo(n);
+        return true;
+
+
+    }
+
+    public boolean removePosition(int pos){
+      
+        if (isEmpty()){
+            return false;
+        }
+        Nodo p1 = radice;
+        Nodo p2 = radice.getSuccessivo();
+        for(int i=0; i<=pos || p1.getSuccessivo()==null; i++){
+            p2 = p1.getSuccessivo();
+            p1 = p2;
+               }
+               p1.setSuccessivo(p2.getSuccessivo());
+                return true;
+
+    }
+    public boolean removValue(int valore) {
+        if (isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    
+
+
+        
+         
+
+
+
+
            
             
 
--- LLista/Main.java
+++ LLista/Main.java
@@ -1,29 +1,33 @@
 package LLista;
 
 public class Main {
-    
-    public static void main (String[] args) {
-        System.out.println("Partiti!");
-        
-        Lista l = new Lista();
-        System.out.println( l );
 
-        /*
-        for (int i=0; i<10; i++) { 
-            l.addTail( new Nodo( i, null ));
+    public static void main(String[] args) {
+        System.out.println("Partiti!");
+
+        Lista l = new Lista();
+        System.out.println(l);
+
+       /*  for (int i = 0; i < 10; i++) {
+            l.addTail(new Nodo(i, null));
         }
-        l.addHead( new Nodo(2024, null ));
+        l.addHead(new Nodo(2024, null));
         */
+
+        l.addSorted(new Nodo(30, null));
         l.addSorted(new Nodo(300, null));
         l.addSorted(new Nodo(320, null));
-        l.addSorted(new Nodo(30, null));
-        l.addSorted(new Nodo(234, null));
+        l.addSorted(new Nodo(330, null));
         l.addSorted(new Nodo(378, null));
         l.addSorted(new Nodo(567, null));
+        l.addAfter(new Nodo(600, null), 3);
+        l.removePosition(2);
+        System.out.println(l);
 
-        System.out.println( l );
-    } 
+
+
+        
+        
+    }
 
 }
-    
-
