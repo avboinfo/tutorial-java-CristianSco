1e3e2e2c1632d80a6a4163a3779ed38cc18977ea
Crisctiann <150423871+CristianSco@users.noreply.github.com>
2024-04-04 10:35:10 +0000

    gen

---
+++ LLista/MainGen.java
@@ -0,0 +1,9 @@
+package LLista;
+
+public class MainGen {
+    public static void main( String[] args ){
+        NodoGen n = new NodoGen<>("forza juve");
+
+        System.out.println(n);
+    }
+}
