bf51161429a3a5c931dfc377e798caa122b28e7f
Crisctiann <150423871+CristianSco@users.noreply.github.com>
2024-05-03 08:40:00 +0000

    d

--- Alberi/Main.java
+++ Alberi/Main.java
@@ -1,5 +1,16 @@
+
 public class Main {
     public static void main(String[] args) {
     
+        Nodo radice= new Nodo(10);
+
+        Nodo pippo= new Nodo(3);
+        Nodo pluto= new Nodo(5);
+        radice.setFiglio_dx(pippo);
+        radice.setFiglio_sx(pluto);
+
+        System.out.println(radice);
 }
+
+
 }
--- Alberi/Nodo.java
+++ Alberi/Nodo.java
@@ -29,4 +29,29 @@ public class Nodo{
     public void setFiglio_sx(Nodo figlio_sx) {
         this.figlio_sx = figlio_sx;
     }
+    public double getValore() {
+        return valore;
+    }
+    public void setValore(double valore) {
+        this.valore = valore;
+    }
+    public String toString() {
+
+      
+        return String.valueOf(valore);
+
+        
+        
+        /*return "Nodo{" +
+                "figlio_dx=" + figlio_dx +
+                ", figlio_sx=" + figlio_sx +
+                ", valore=" + valore +
+                '}';
+    */
+    }  
+    
+    boolean eFoglia(){
+        return (figlio_dx==null && figlio_sx==null);
+    }
+
 }
